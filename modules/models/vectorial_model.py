eb5b4b64b9728f55cacede1a1fa0562bb10eab49
Rodrigo43 <rodrigo.garcia21111@gmail.com>
2022-06-26 19:16:35 -0400

    Add levenshtein distance split frequencies in categories worse results than before

--- modules/models/vectorial_model.py
+++ modules/models/vectorial_model.py
@@ -1,6 +1,7 @@
 from itsdangerous import exc
 import modules.data_processor.normalizer as normalizer
 import modules.data_processor.tokenizer as tokenizer
+from modules.utils.levenshtein import levenshtein
 import math
 import json
 
@@ -10,6 +11,7 @@ class VectorialModel:
         self.recover_amount = recover_amount
         self.terms = {}
         self.data_size = 0
+        self.items = {}
         
         
 
@@ -17,50 +19,60 @@ class VectorialModel:
     def add_data(self, items:list):
         self.data_size = len(items)
         total_terms_by_id = {}
+        terms = {}
         for item in items:
+            self.items[item.id] = item
             normalized_title = normalizer.normalize(tokenizer.tokenize(item.title))
             normalized_text = normalizer.normalize(tokenizer.tokenize(item.text))
             normalized_author = normalizer.normalize(tokenizer.tokenize(item.author))
             normalized_bib = normalizer.normalize(tokenizer.tokenize(item.bib))
 
-            total_terms_by_id[item.id] = (len(normalized_title) +
-                                            len(normalized_text) +
-                                            len(normalized_author) +
-                                            len(normalized_bib))
+            total_terms_by_id[(item.id, "ti")] = len(normalized_title) 
+            total_terms_by_id[(item.id, "tx")] = len(normalized_text) 
+            total_terms_by_id[(item.id, "au")] = len(normalized_author) 
+            total_terms_by_id[(item.id, "bi")] = len(normalized_bib)
 
-            self.add(item.id, normalized_title)
-            self.add(item.id, normalized_text)
-            self.add(item.id, normalized_author)
-            self.add(item.id, normalized_bib)
+            self.add(item.id, normalized_title, "ti", terms)
+            self.add(item.id, normalized_text, "tx", terms)
+            self.add(item.id, normalized_author, "au", terms)
+            self.add(item.id, normalized_bib, "bi", terms)
 
         # total = len(self.terms)
         # print(total)
 
-        for term, docs in self.terms.items():
-            for doc, freq in docs.items():
-                tf = freq / total_terms_by_id[doc]
+        for term, docs in terms.items():
+            for (doc, type), freq in docs.items():
+                tf = freq / total_terms_by_id[(doc, type)]
                 idf = math.log(len(items)/len(docs))
-                docs[doc] = (tf * idf)
+                self.terms[term][doc] = (tf * idf)
             # print(term)
             # print(docs)
             # print("+++++++++++++++++++++++++++++++++")
 
 
-    def add(self, id, words):
+    def add(self, id, words, type, terms):
         for w in words:
-            if w in (self.terms):
+            if w in (terms):
                 try:
-                    self.terms[w][id] += 1
+                    terms[w][(id,type)] += 1
                 except KeyError:
-                    self.terms[w][id] = 1
+                    terms[w][(id,type)] = 1
             else:
-                self.terms[w] = {id:1}
+                terms[w] = {(id,type):1}
+                self.terms[w] = {}
 
 
     def make_query(self, query):
-        normalized_query = normalizer.normalize(tokenizer.tokenize(query))
-        
+        normalized_query = normalizer.normalize(tokenizer.tokenize(query))        
+
         query_vector = {}
+        # for word in normalized_query:
+        #     for w in self.get_nearest_words(word):
+                # try:
+                #     query_vector[w] += 1
+                # except KeyError:
+                #     query_vector[w] = 1
+                
         for w in normalized_query:
             if w not in self.terms:
                 continue
@@ -74,7 +86,7 @@ class VectorialModel:
                 continue
             tf = freq / len(normalized_query)
             idf = math.log(self.data_size/len(self.terms[w]))
-            query_vector[w] = (self.alpha + (1 - self.alpha)) * (tf * idf)
+            query_vector[w] = (self.alpha + ((1 - self.alpha)) * tf) * idf
         
         rank = {}
         for term, q_weight in query_vector.items():
@@ -90,6 +102,15 @@ class VectorialModel:
         rank = rank[: self.recover_amount]
 
         print(rank)
+
+    def get_nearest_words(self,word):
+        
+        result = []
+        for term, _ in self.terms.items():
+            dist = levenshtein(term, word)
+            if dist < 3: 
+                result.append(term)
+        return result
 
     def save(self, path):
         with open(f'{path}/data_from_vectorial_model.json', 'w+') as f:
